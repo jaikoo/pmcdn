c2d09b8bff539a84c34fad51ed24d79d2ff18225
jaikoo <jonathan@jaikoo.com>
2008-05-09 22:36:47 +0000

    updated README

--- app/controllers/assets.rb
+++ app/controllers/assets.rb
@@ -2,10 +2,10 @@ class Assets < Application
   
   def show
     file = request.env['REQUEST_PATH'].gsub('/assets/', '')
-    obj = AWS::S3::S3Object.find(file, 'moo_test' )
+    obj = AWS::S3::S3Object.find(file, 'BUCKET_NAME' )
     stream_file({ :filename =>  file, :type => obj.content_type,
       :content_length => obj.content_length }) do |response|
-      AWS::S3::S3Object.stream(file, 'moo_test') do |chunk|
+      AWS::S3::S3Object.stream(file, 'BUCKET_NAME') do |chunk|
         response.write chunk
       end
     end
